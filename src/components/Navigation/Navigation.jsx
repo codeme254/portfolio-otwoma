477bd739cd33acbd7ba41b9259b9e8e61ee8bb5f
otwoma <otwomadennis365@gmail.com>
2022-04-01 16:43:01 +0300

    making the header responsive

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, {useRef} from "react";
 
 const Navigation = () => {
+    const navRef = useRef(null)
+    const toggleNav = () => {
+        navRef.current.classList.toggle('show-nav')
+    }
     return(
         <header className="header">
             <div className="header__logo">
                 <h2 className="header__logo--text">D.Otwoma</h2>
             </div>
-            <nav className="nav">
+            <nav className="nav" ref={navRef}>
                 <ul className="nav__list">
                     <li className="nav__item"><a href="/" className="nav__item--link">Home</a></li>
                     <li className="nav__item"><a href="/" className="nav__item--link">About me</a></li>
@@ -18,6 +22,11 @@ const Navigation = () => {
                     {/* <li className="nav__item"><a href="/" className="nav__item--link">Another link</a></li> */}
                 </ul>
             </nav>
+            <div className="nav__burger_pr" onClick={toggleNav}>
+                <div className="nav__burger nav__burger--1"></div>
+                <div className="nav__burger nav__burger--2"></div>
+                <div className="nav__burger nav__burger--3"></div>
+            </div>
         </header>
     )
 }
