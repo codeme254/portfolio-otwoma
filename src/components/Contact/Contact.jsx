497f918727c7e7cfc3d657f57a31f56089ce1bac
otwoma <otwomadennis365@gmail.com>
2022-04-05 18:15:49 +0300

    created the form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Heading from "../Heading/Heading";
 
 const Contact = () => {
     return(
-        <section className="contact">
+        <section className="contact-section">
+            <Heading title="contact" subTitle="Let's talk" />
             <div className="form__wraper">
                 <div className="details__wrapper">
                     <div className="detail">
@@ -19,21 +21,26 @@ const Contact = () => {
                     </div>
                 </div>
                 <form action="/" className="form">
-                    <div className="form__group">
-                        <label htmlFor="name" className="form__group--label">full name</label>
-                        <input type="text" className="form__group--input" placeholder="full name" required />
+                    <div className="form__flex">
+                        <div className="form__group form__group--half">
+                            {/* <label htmlFor="name" className="form__group--label">full name</label> */}
+                            <input type="text" className="form__group--input" placeholder="full name" required />
+                        </div>
+                        <div className="form__group form__group--half">
+                            {/* <label htmlFor="name" className="form__group--label">email address</label> */}
+                            <input type="email" className="form__group--input" placeholder="email address" required />
+                        </div>
                     </div>
                     <div className="form__group">
-                        <label htmlFor="name" className="form__group--label">email address</label>
-                        <input type="email" className="form__group--input" placeholder="email address" required />
-                    </div>
-                    <div className="form__group">
-                        <label htmlFor="name" className="form__group--label">subject</label>
-                        <input type="email" className="form__group--input" placeholder="email address" required />
+                        {/* <label htmlFor="name" className="form__group--label">subject</label> */}
+                        <input type="text" className="form__group--input" placeholder="subject" required />
                     </div>
                     <div className="form__group">
                         <textarea placeholder="your message here" required className="form__textarea"></textarea>
                     </div>
+                    <p className="form__legend">
+                        Am also open to suggestions, so please feel to send your critics.
+                    </p>
                     <div className="form__group">
                         <button className="form__button" type="submit">submit</button>
                     </div>
