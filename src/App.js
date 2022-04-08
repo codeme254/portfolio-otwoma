f1a0b16e79454b68a527af5cb65e97f0cedbfe5b
otwoma <otwomadennis365@gmail.com>
2022-04-08 11:20:09 +0300

    added page load animations

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState, useEffect} from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Home from './components/Home/Home'
 import About from './components/About/About'
@@ -8,17 +9,38 @@ import Contact from './components/Contact/Contact';
 import Jokes from './components/Jokes/Jokes';
 import Footer from './components/Footer/Footer';
 
+import RiseLoader from "react-spinners/RiseLoader";
+
 function App() {
+  const color = '#0d86f8';
+  const [loading, setLoading ] = useState(false)
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 6000)
+  }, [])
   return (
     <div className="App">
-      <Navigation />
-      <Home />
-      <About />
-      <Services />
-      <Projects />
-      <Contact />
-      <Jokes />
-      <Footer />
+      {
+        loading ? 
+        <div className='loader'>
+          <h1 className='loader__title'>Meet Otwoma Dennis</h1>
+          <RiseLoader color={color} loading={loading}  size={50} />
+          <p className='loader__paragraph'>A frontend web developer, backend web developer, python programmer, part time cyclist and proffesional dreamer.</p>
+        </div>
+         : 
+        <>
+          <Navigation />
+          <Home />
+          <About />
+          <Services />
+          <Projects />
+          <Contact />
+          <Jokes />
+          <Footer />
+        </>
+      }
     </div>
   );
 }
